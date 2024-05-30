ea6cb78e3d2d2eaeadc9ef7c8d52920d0466ec4c
alexb997 <alex_alexandru_997@yahoo.ro>
2024-05-30 21:34:17 +0300

    intermediary commit

--- portofolio-front/src/views/ProjectsView.vue
+++ portofolio-front/src/views/ProjectsView.vue
@@ -12,7 +12,21 @@
         style="height: 300px"
       >
         <router-link :to="`/project/${project.id}`">
-          {{ project.name }}
+          <div class="card h-100" :title="project.name">
+            <div
+              class="card-img"
+              :src="project.imgUrl"
+              alt="Project image"
+            ></div>
+            <div class="card-text">
+              <div class="mb-2">
+                <strong>Description:</strong> {{ project.description }}
+              </div>
+              <div class="mb-2">
+                <strong>Status:</strong> {{ project.status }}
+              </div>
+            </div>
+          </div>
         </router-link>
       </div>
     </div>
