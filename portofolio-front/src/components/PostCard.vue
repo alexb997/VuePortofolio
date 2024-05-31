592e022b40766ec19f850f38e7ad07625944aec7
alexb997 <alex_alexandru_997@yahoo.ro>
2024-05-31 23:01:12 +0300

    intermediary commit

---
+++ portofolio-front/src/components/PostCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div 
+              no-body
+              bg-variant="dark"
+              text-variant="white"
+              class="overflow-hidden card m-2"
+              style="height: 100px;"
+            >
+              <row no-gutters>
+                <div class="card-body">
+                  <h5 class="card-title">{{post.title}}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{{post.type}} </h6>
+                  <p class="card-text">{{post.description }}</p>
+                  <hr />
+                </div>
+              </col>
+            </row>
+          </div>
+</template>
+
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- portofolio-front/src/views/ProjectDetailsView.vue
+++ portofolio-front/src/views/ProjectDetailsView.vue
@@ -2,41 +2,34 @@
   <div>
     <div class="container">
       <div class="col infoZone mt-5 mb-4">
-        <b-img :src="this.imgUrl" class="pt-4" fluid></b-img>
-        <h3>{{ this.name }}</h3>
+        <b-img :src="project.imgUrl" class="pt-4" fluid></b-img>
+        <h3>{{ project.name }}</h3>
         <div class="row p-1">
-          <label>Name: {{ this.name }}</label>
+          <label>Name: {{ project.name }}</label>
         </div>
         <div class="row p-1">
-          <label>Description :{{ this.description }}</label>
+          <label>Description :{{ project.description }}</label>
         </div>
         <div class="row p-1">
-          <label>ImgUrl :{{ this.imgUrl }}</label>
+          <label>ImgUrl :{{ project.imgUrl }}</label>
         </div>
         <div class="row p-1">
-          <label>Status :{{ this.imgUrl }}</label>
+          <label>Status :{{ project.imgUrl }}</label>
         </div>
       </div>
       <h3>Related Posts</h3>
-      <div class="container p-3" v-if="posts && posts.length">
-        <b-col>
-          <b-card
-            class="m-3 bCard"
-            v-for="post in posts"
-            v-bind:key="post.id"
-            :title="post.title"
-            :sub-title="post.type"
-          >
-            <b-card-text>
-              {{ post.description }}
-            </b-card-text>
-          </b-card>
-        </b-col>
+      <div class="container p-3" v-if="project.posts && project.posts.length">
+        <PostCard
+          v-for="(post, index) in project.posts"
+          :key="index"
+          :post="post"
+        >
+        </PostCard>
       </div>
     </div>
     <div class="pb-5">
       <h3>Add a post:</h3>
-      <a :href="this.id + `/addPost`"
+      <a :href="project.id + `/addPost`"
         >Redirect(Adauga trimitere proiect la componenta creeare post)</a
       >
     </div>
@@ -44,5 +37,38 @@
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { useRoute } from "vue-router";
+import PostCard from "@/components/PostCard.vue";
 
+export default {
+  name: "ProjectDetails",
+  components: {
+    PostCard,
+  },
+  setup() {
+    const project = ref({});
+    const route = useRoute();
+
+    const fetchProject = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/project/${route.params.id}`
+        );
+        project.value = response.data;
+      } catch (err) {
+        console.error("Failed to fetch project", err);
+      }
+    };
+
+    onMounted(() => {
+      fetchProject();
+    });
+
+    return {
+      project,
+    };
+  },
+};
 </script>
